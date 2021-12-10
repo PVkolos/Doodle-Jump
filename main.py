a6d9a9606ddad9715eed2ce83362bf85b9a2b257
bzorn <v.klimenko.2137@gmail.com>
2021-12-10 18:43:41 +0300

    .

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import pygame
 import random
 
 
-class Boost:
-    def __init__(self, x, y, width, screen):
-        self.coord = (x, y)
-        self.screen = screen
-        self.width = width
-        self.image = pygame.image.load('images/green.png')
-
-    def draw(self):
-        self.screen.blit(self.image, (self.coord[0] - self.width / 2, self.coord[1]))
-        pygame.draw.line(self.screen, 'green', (self.coord[0] - self.width / 2, self.coord[1]), (self.coord[0] + self.width / 2, self.coord[1]), width=5)
-
-
 class Player:
     def __init__(self, screen):
         self.y = 400
@@ -28,9 +16,10 @@ class Player:
         self.screen = screen
         self.is_jump = 0
 
-    def down(self, boost):
-        for el in boost:
-            if ((el[0] - 40 <= self.x <= el[0] + 55) or (el[0] - 40 <= self.x + self.width <= el[0] + 55)) and self.y == el[1] and not self.jump:
+    def down(self, boosts):
+        for el in boosts:
+            if ((el.x - 40 <= self.x <= el.x + 55) or (el.x - 40 <= self.x + self.width <= el.x + 55)) and self.y == \
+                    el.y and not self.jump:
                 self.jump = True
                 self.is_jump = 200
         if not self.jump:
@@ -39,16 +28,27 @@ class Player:
             self.jump = False
         elif self.jump:
             if self.y <= 400:
-                for el in boost:
-                    el[1] += 5
+                for el in boosts:
+                    el.y += 5
                 self.y += 5
             self.y -= 5
             self.is_jump -= 5
-            if self.image == self.pl_right and self.is_jump > 100: self.image = self.pl_right_pr
-            elif self.image == self.pl_left and self.is_jump > 100: self.image = self.pl_left_pr
-            elif self.is_jump <= 100 and self.image == self.pl_right_pr: self.image = self.pl_right
-            elif self.is_jump <= 100 and self.image == self.pl_left_pr: self.image = self.pl_left
+            if self.image == self.pl_right and self.is_jump > 100:
+                self.image = self.pl_right_pr
+            elif self.image == self.pl_left and self.is_jump > 100:
+                self.image = self.pl_left_pr
+            elif self.is_jump <= 100 and self.image == self.pl_right_pr:
+                self.image = self.pl_right
+            elif self.is_jump <= 100 and self.image == self.pl_left_pr:
+                self.image = self.pl_left
         self.screen.blit(self.image, (self.x, self.y - 82))
+
+
+class Boost:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 80
 
 
 class App:
@@ -56,8 +56,8 @@ class App:
         pygame.init()
         self.screen = pygame.display.set_mode((600, 800))
         self.screen.set_alpha(None)
-        self.bg = pygame.image.load("images/Фон уровней.jpg")
-        self.boosts = [[100, 750, 80], [300, 750, 80], [500, 750, 80]]
+        self.bg = pygame.image.load("images/bg.jpg")
+        self.boosts = [Boost(100, 750), Boost(300, 750), Boost(500, 750)]
         self.pl = Player(self.screen)
         self.clock = pygame.time.Clock()
         self.image_boost = pygame.image.load("images/green.png").convert_alpha()
@@ -66,21 +66,21 @@ class App:
         pygame.display.set_caption('DoodleJumpDemo')
 
     def draw(self, boosts):
-        for coord in boosts:
-            self.screen.blit(self.image_boost, (coord[0] - 60 / 2, coord[1]))
+        for boost in boosts:
+            self.screen.blit(self.image_boost, (boost.x - 60 / 2, boost.y))
 
     def check_play(self):
         if len(self.boosts) < 15:
             for i in range(15 - len(self.boosts)):
-                self.coord = (random.randint(80, 600 - 80), random.randrange(round(self.boosts[-1][1] - 150), round(self.boosts[-1][1]), 5))
-                self.boosts.append([self.coord[0], self.coord[1], 80])
+                coord = (random.randint(80, 600 - 80), random.randrange(round(self.boosts[-1].y - 150), round(self.boosts[-1].y), 5))
+                self.boosts.append(Boost(coord[0], coord[1]))
         if self.pl.x < -80:
             self.pl.x = 580
         elif self.pl.x > 680:
             self.pl.x = -40
         a = self.boosts.copy()
         for i in range(len(a)):
-            if a[i][1] > 800:
+            if a[i].y > 800:
                 del self.boosts[i]
 
     def get_fps(self):
@@ -102,14 +102,6 @@ class App:
             self.screen.blit(text2, (250, 250))
             pygame.display.flip()
 
-    def functions(self):
-        self.clock.tick(60)
-        self.check_play()
-        self.screen.blit(self.bg, (0, 0))
-        self.draw(self.boosts)
-        self.pl.down(self.boosts)
-        self.get_fps()
-
     def start(self):
         self.running = True
         while self.running:
@@ -119,12 +111,19 @@ class App:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]:
                 self.pl.image = self.pl.pl_left
-                self.pl.x -= 3
+                self.pl.x -= 5
             if keys[pygame.K_RIGHT]:
                 self.pl.image = self.pl.pl_right
-                self.pl.x += 3
-            self.functions()
-            if self.pl.y > 800: self.flag = False; break
+                self.pl.x += 5
+            self.clock.tick(60)
+            self.check_play()
+            self.screen.blit(self.bg, (0, 0))
+            self.draw(self.boosts)
+            self.pl.down(self.boosts)
+            self.get_fps()
+            if self.pl.y > 800:
+                self.flag = False
+                break
             pygame.display.flip()
 
         if not self.flag:
@@ -137,4 +136,3 @@ class App:
 if __name__ == '__main__':
     app = App()
     app.start()
-
