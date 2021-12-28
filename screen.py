97b255722571503d38a06ec31f89dc8ddc9f1ba5
bzorn <v.klimenko.2137@gmail.com>
2021-12-28 16:45:21 +0300

    файл с классами для разных экранов

---
+++ screen.py
@@ -0,0 +1,18 @@
+class Screen:
+    def __init__(self):
+        pass
+
+
+class Pause(Screen):
+    def __init__(self):
+        super().__init__()
+
+
+class GameOver(Screen):
+    def __init__(self):
+        super().__init__()
+
+
+class Start(Screen):
+    def __init__(self):
+        super().__init__()
