b9fd950cad6f00752c0823f76f59f8b2794b779c
bzorn <v.klimenko.2137@gmail.com>
2022-01-19 20:37:00 +0300

    .

--- boost.py
+++ boost.py
@@ -81,9 +81,6 @@ class FederBoost(Boost):
     def jump(self):
         self.is_feder = True
 
-    def jump(self):
-        self.is_feder = True
-
 
 class MovementBoost(Boost):
     def __init__(self, x, y, *groups):
